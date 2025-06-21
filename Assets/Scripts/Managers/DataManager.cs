939215008125c2b8f1a4844c2d70a3cb9023d58a
Sudo0928 <57037151+Sudo0928@users.noreply.github.com>
2025-06-21 22:01:38 +0900

    [feat] DataManager 및 UserData 예제

--- Assets/Scripts/Managers/DataManager.cs
+++ Assets/Scripts/Managers/DataManager.cs
@@ -4,5 +4,10 @@ using UnityEngine;
 
 public class DataManager
 {
-    
+    public GameInfo_DragonsDataLoader GameInfo;
+
+    public void Init()
+    {
+        GameInfo = new GameInfo_DragonsDataLoader();
+    }
 }
