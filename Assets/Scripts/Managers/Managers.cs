03153b780b63fa6e5fa62afd9f3d682366c1cd69
Sudo0928 <57037151+Sudo0928@users.noreply.github.com>
2025-06-21 22:01:38 +0900

    [feat] DataManager 및 UserData 예제

--- Assets/Scripts/Managers/Managers.cs
+++ Assets/Scripts/Managers/Managers.cs
@@ -11,13 +11,42 @@ public class Managers : Singleton<Managers>
     [SerializeField] private PoolManager poolManager = new PoolManager();
     [SerializeField] private SoundManager soundManager = new SoundManager();
     [SerializeField] private UIManager uiManager = new UIManager();
-    [SerializeField] public ResourceManager resourceManager = new ResourceManager();
+    [SerializeField] private ResourceManager resourceManager = new ResourceManager();
+    [SerializeField] private DataManager dataManager = new DataManager();
+    [SerializeField] private UserData userData = new UserData();
 
     public static PoolManager Pool => Instance.poolManager;
     public static SoundManager Sound => Instance.soundManager;
     public static UIManager UI => Instance.uiManager;
     public static ResourceManager Resource => Instance.resourceManager;
+    public static DataManager Data => Instance.dataManager;
+    public static UserData UserData => Instance.userData;
 
-    
+    protected override void Awake()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        dataManager.Init();
+
+        Load();
+    }
+
+    [ContextMenu("Save")]
+    public void Save()
+    {
+        PlayerPrefs.SetString("UserData", JsonUtility.ToJson(userData));
+    }
+
+    [ContextMenu("Load")]
+    public void Load()
+    {
+        if (PlayerPrefs.HasKey("UserData"))
+        {
+            JsonUtility.FromJsonOverwrite(PlayerPrefs.GetString("UserData"), userData);
+        }
+    }
 }
 
